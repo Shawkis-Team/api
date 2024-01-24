6c5ee97e66e8381b7bfb8cab06313dbf255927be
Kalanji <djiga2015@gmail.com>
2024-01-24 22:57:16 +0100

    daxx

--- src/module/ticket/service.ts
+++ src/module/ticket/service.ts
@@ -17,18 +17,28 @@ export class TicketService {
     return ExceptionCode.SUCCEEDED;
   }
   async getByRef(ref: string) {
-    const ticket = await this.repos.findOne({
-      where: { refTicket: Equal(ref) },
-      relations: { compte: true, carte: true },
-    });
-    console.log(ticket);
-    if (!ticket) throw new HttpException(ExceptionCode.NOT_FOUND, 404);
+    return this.repos
+      .findOne({
+        where: { refTicket: Equal(ref) },
+        relations: { compte: true, carte: { compte: true } },
+      })
+      .then((ticket) => {
+        console.log(ticket);
+        if (!ticket) throw new HttpException(ExceptionCode.NOT_FOUND, 404);
 
-    if (ticket.status !== 0)
-      throw new HttpException(
-        { ...ExceptionCode.NOT_FOUND, message: 'ticket invalid' },
-        404,
-      );
-    return ticket;
+        if (ticket.status !== 0)
+          throw new HttpException(
+            { ...ExceptionCode.NOT_FOUND, message: 'ticket invalid' },
+            404,
+          );
+        return { ...ticket, compte: ticket.compte ?? ticket.carte.compte };
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new HttpException(
+          { ...ExceptionCode.NOT_FOUND, message: 'ticket invalid' },
+          404,
+        );
+      });
   }
 }
